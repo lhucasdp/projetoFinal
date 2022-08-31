3bc365d1b557dd7fbb6559054acd60188448bac4
unknown <lhucasdp@gmail.com>
2022-08-31 02:17:51 -0300

    atualizaaaa

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import VueAxios from 'vue-axios';
 
 axios.defaults.params = {
     language: "pt-BR",
-    api_key: "a6e6381bba3f98f3d8d5d40dbe25d1a4"
+    api_key: "" //Coloque sua chave dentro das aspas
 }
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
@@ -12,9 +12,3 @@ axios.defaults.baseURL = "https://api.themoviedb.org/3";
 Vue.use(
     VueAxios,axios
 );
-
-// install(Vue){
-//     Vue.prototype.$http = axios;
-// },
-
-// https://api.themoviedb.org/3/movie/550?api_key=a16fec3182959f245e5479f51cace100
